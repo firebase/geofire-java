2b216ec3143189666e8c9d0e3a54decef5ee5c99
Jonny Dimond <jonny@firebase.com>
2014-07-21 16:22:18 +0200

    added sf vehicles example

---
+++ examples/SFVehicles/SF Vehicles/src/main/java/com/firebase/sfvehicles/SFVehiclesActivity.java
@@ -0,0 +1,142 @@
+package com.firebase.sfvehicles;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.app.FragmentActivity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+import com.firebase.client.Firebase;
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SFVehiclesActivity extends FragmentActivity implements GeoQueryEventListener, GoogleMap.OnCameraChangeListener {
+
+    private static final LatLng INITIAL_CENTER = new LatLng(37.7789, -122.4017);
+    private static final int INITIAL_ZOOM_LEVEL = 14;
+    private static final String GEO_FIRE_REF = "https://geofire-ios.firebaseio.com/geofire";
+
+    private GoogleMap map;
+    private Circle searchCircle;
+    private GeoFire geoFire;
+    private GeoQuery geoQuery;
+
+    private Map<String,Marker> markers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sfvehicles);
+
+        // setup map and camera position
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        this.map = mapFragment.getMap();
+        this.searchCircle = this.map.addCircle(new CircleOptions().center(INITIAL_CENTER).radius(1000));
+        this.searchCircle.setFillColor(Color.argb(66, 255, 0, 255));
+        this.searchCircle.setStrokeColor(Color.argb(66, 0, 0, 0));
+        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(INITIAL_CENTER, INITIAL_ZOOM_LEVEL));
+        this.map.setOnCameraChangeListener(this);
+
+        // setup GeoFire
+        this.geoFire = new GeoFire(new Firebase(GEO_FIRE_REF));
+        this.geoQuery = this.geoFire.queryAtLocation(INITIAL_CENTER.latitude, INITIAL_CENTER.longitude, 1000);
+
+        // setup markers
+        this.markers = new HashMap<String, Marker>();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // remove all event listeners to stop updating in the background
+        this.geoQuery.removeAllEventListeners();
+        for (Marker marker: this.markers.values()) {
+            marker.remove();
+        }
+        this.markers.clear();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // add an event listener to start updating locations again
+        this.geoQuery.addGeoQueryEventListener(this);
+    }
+
+    @Override
+    public void onKeyEntered(String key, double lat, double lng) {
+        // Add a new marker to the map
+        Marker marker = this.map.addMarker(new MarkerOptions().position(new LatLng(lat, lng)));
+        this.markers.put(key, marker);
+    }
+
+    @Override
+    public void onKeyExited(String key) {
+        // Remove any old marker
+        Marker marker = this.markers.get(key);
+        if (marker != null) {
+            marker.remove();
+            this.markers.remove(key);
+        }
+    }
+
+    @Override
+    public void onKeyMoved(String key, double lat, double lng) {
+        // Move the marker
+        Marker marker = this.markers.get(key);
+        if (marker != null) {
+            this.animateMarkerTo(marker, lat, lng);
+        }
+    }
+
+    // Animation handler for old APIs without animation support
+    private void animateMarkerTo(final Marker marker, final double lat, final double lng) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final long DURATION_MS = 3000;
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+        final LatLng startPosition = marker.getPosition();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                float elapsed = SystemClock.uptimeMillis() - start;
+                float t = elapsed/DURATION_MS;
+                float v = interpolator.getInterpolation(t);
+
+                double currentLat = (lat - startPosition.latitude) * v + startPosition.latitude;
+                double currentLng = (lng - startPosition.longitude) * v + startPosition.longitude;
+                marker.setPosition(new LatLng(currentLat, currentLng));
+
+                // if animation is not finished yet, repeat
+                if (t < 1) {
+                    handler.postDelayed(this, 16);
+                }
+            }
+        });
+    }
+
+    private double zoomLevelToRadius(double zoomLevel) {
+        // Approximation to fit circle into view
+        return 16384000/Math.pow(2, zoomLevel);
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+        // Update the search criteria for this geoQuery and the circle on the map
+        LatLng center = cameraPosition.target;
+        double radius = zoomLevelToRadius(cameraPosition.zoom);
+        this.searchCircle.setCenter(center);
+        this.searchCircle.setRadius(radius);
+        this.geoQuery.setCenter(center.latitude, center.longitude);
+        this.geoQuery.setRadius(radius);
+    }
+}
