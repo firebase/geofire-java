e8647cea94660e7d3d7def5418fc7dc6bcc474da
Jonah Dahlquist <jonah@nucleussystems.com>
2016-12-20 17:55:08 -0800

    Fixed compatibility with GAE, sacrificed Android compatibility

--- src/main/java/com/firebase/geofire/AppEngineEventRaiser.java
+++ src/main/java/com/firebase/geofire/AppEngineEventRaiser.java
@@ -3,10 +3,14 @@ package com.firebase.geofire;
 import com.google.appengine.api.ThreadManager;
 import java.util.concurrent.Executors;
 
-class AppEngineEventRaiser extends ThreadEventRaiser {
+class AppEngineEventRaiser implements EventRaiser {
 
-    @Override
     public AppEngineEventRaiser() {
         this.executorService = Executors.newSingleThreadExecutor(ThreadManager.backgroundThreadFactory());
     }
+
+    public void raiseEvent(Runnable r) {
+        this.executorService.submit(r);
+    }
 }
+
--- src/main/java/com/firebase/geofire/GeoFire.java
+++ src/main/java/com/firebase/geofire/GeoFire.java
@@ -122,12 +122,9 @@ public class GeoFire {
         this.databaseReference = databaseReference;
         EventRaiser eventRaiser;
         try {
-            eventRaiser = new AndroidEventRaiser();
-        } catch (Throwable e) {
-            // We're not on Android, try using GAE-specific event raiser
             eventRaiser = new AppEngineEventRaiser();
         } catch (Throwable e) {
-            // We're not on Android or GAE, use the ThreadEventRaiser
+            // We're not on GAE, use the ThreadEventRaiser
             eventRaiser = new ThreadEventRaiser();
         }
         this.eventRaiser = eventRaiser;
