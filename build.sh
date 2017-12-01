e6adea9eecf9540e0acbd668f70931aeacc4bda3
Sam <samstern@google.com>
2017-12-01 14:29:45 -0800

    Better environment detection

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Work off travis
+# Exit on error
+set -e
+
+# Set travis environment variables even when run locally
+
+# 1) Pull request
 if [[ ! -z TRAVIS_PULL_REQUEST ]]; then
   echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 else
@@ -8,14 +13,19 @@ else
   TRAVIS_PULL_REQUEST=false
 fi
 
-# Exit on error
-set -e
+# 2) Secure env variables
+if [[ ! -z TRAVIS_SECURE_ENV_VARS ]]; then
+  echo "TRAVIS_SECURE_ENV_VARS: $TRAVIS_SECURE_ENV_VARS"
+else
+  echo "TRAVIS_SECURE_ENV_VARS: unset, setting to false"
+  TRAVIS_SECURE_ENV_VARS=false
+fi
 
 # Build
 mvn clean compile
 
-# Only run test suite for non-PR builds
-if [ $TRAVIS_PULL_REQUEST = false ] ; then
+# Only run test suite when we can decode the service acct
+if [ $TRAVIS_SECURE_ENV_VARS] ; then
   # Decrypt service account
   openssl aes-256-cbc -K $encrypted_d7b8d9290299_key -iv $encrypted_d7b8d9290299_iv \
     -in java/service-account.json.enc -out java/service-account.json -d
