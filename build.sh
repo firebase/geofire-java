933df2360ea79dcc798b2a29cdf9bfc33c3a8ca7
Sam <samstern@google.com>
2017-12-01 14:23:31 -0800

    Move decrypt to build script

--- build.sh
+++ build.sh
@@ -16,5 +16,10 @@ mvn clean compile
 
 # Only run test suite for non-PR builds
 if [ $TRAVIS_PULL_REQUEST = false ] ; then
+  # Decrypt service account
+  openssl aes-256-cbc -K $encrypted_d7b8d9290299_key -iv $encrypted_d7b8d9290299_iv \
+    -in java/service-account.json.enc -out java/service-account.json -d
+
+  # Run test suite
   mvn -Dsurefire.rerunFailingTestsCount=2 -Dsurefire.useFile=false test
 fi
