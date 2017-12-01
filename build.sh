81a521cbdc26bbe4aca562dfb05611296a72b2cc
Sam <samstern@google.com>
2017-12-01 14:21:10 -0800

    Special PR builds

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Work off travis
+if [[ ! -z TRAVIS_PULL_REQUEST ]]; then
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+else
+  echo "TRAVIS_PULL_REQUEST: unset, setting to false"
+  TRAVIS_PULL_REQUEST=false
+fi
+
 # Exit on error
 set -e
 
-# Run test suite
+# Build
 mvn clean compile
-mvn -Dsurefire.rerunFailingTestsCount=2 -Dsurefire.useFile=false test
+
+# Only run test suite for non-PR builds
+if [ $TRAVIS_PULL_REQUEST = false ] ; then
+  mvn -Dsurefire.rerunFailingTestsCount=2 -Dsurefire.useFile=false test
+fi
