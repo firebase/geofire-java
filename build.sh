4eb4bc149689138c8b3acb7441c454be9578798e
Sam Stern <samstern@google.com>
2017-12-12 09:57:32 -0800

    Fix travis build script (#105)

--- build.sh
+++ build.sh
@@ -6,26 +6,28 @@ set -e
 # Set travis environment variables even when run locally
 
 # 1) Pull request
-if [[ ! -z TRAVIS_PULL_REQUEST ]]; then
-  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
-else
+if [ -z "$TRAVIS_PULL_REQUEST" ]; then
   echo "TRAVIS_PULL_REQUEST: unset, setting to false"
   TRAVIS_PULL_REQUEST=false
+else
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 fi
 
 # 2) Secure env variables
-if [[ ! -z TRAVIS_SECURE_ENV_VARS ]]; then
-  echo "TRAVIS_SECURE_ENV_VARS: $TRAVIS_SECURE_ENV_VARS"
-else
+if [ -z "$TRAVIS_SECURE_ENV_VARS" ]; then
   echo "TRAVIS_SECURE_ENV_VARS: unset, setting to false"
   TRAVIS_SECURE_ENV_VARS=false
+else
+  echo "TRAVIS_SECURE_ENV_VARS: $TRAVIS_SECURE_ENV_VARS"
 fi
 
 # Build
 mvn clean compile
 
 # Only run test suite when we can decode the service acct
-if [ $TRAVIS_SECURE_ENV_VARS ] ; then
+if [ "$TRAVIS_SECURE_ENV_VARS" = false ]; then
+  echo "Could not find secure environment variables, not running tests."
+else
   # Decrypt service account
   openssl aes-256-cbc -K $encrypted_d7b8d9290299_key -iv $encrypted_d7b8d9290299_iv \
     -in java/service-account.json.enc -out java/service-account.json -d
