ed2858cf59bbd3c0a66bcc59767a5616ef24c60f
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-12-12 19:44:46 +0100

    Simplify SimpleFuture, move to test (#104)

--- src/test/java/com/firebase/geofire/util/ReadFuture.java
+++ src/test/java/com/firebase/geofire/util/ReadFuture.java
@@ -1,6 +1,5 @@
 package com.firebase.geofire.util;
 
-import com.firebase.geofire.core.SimpleFuture;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Query;
---
+++ src/test/java/com/firebase/geofire/util/SimpleFuture.java
@@ -0,0 +1,43 @@
+package com.firebase.geofire.util;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SimpleFuture<V> {
+    private V value;
+    private boolean isSet;
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+
+    public synchronized void put(final V valueToPut) {
+        lock.lock();
+
+        try {
+            value = valueToPut;
+            isSet = true;
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public V get(final long timeout, final TimeUnit unit) throws InterruptedException, TimeoutException {
+        lock.lock();
+
+        try {
+            while (!isSet) {
+                if (!condition.await(timeout, unit)) {
+                    throw new TimeoutException();
+                }
+            }
+
+            return value;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
