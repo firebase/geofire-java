72945e91b8b6b8dc7a1b53c743ee0d83df78ad47
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-12-18 13:19:25 +0100

    Use StringBuilder instead of StringBuffer.

--- src/test/java/com/firebase/geofire/TestHelpers.java
+++ src/test/java/com/firebase/geofire/TestHelpers.java
@@ -8,12 +8,12 @@ public final class TestHelpers {
     private static final String ALPHA_NUM_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789";
 
     public static String randomAlphaNumericString(int length) {
-        StringBuffer buffer = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         Random random = new Random();
         for (int i = 0; i < length; i++ ) {
-            buffer.append(ALPHA_NUM_CHARS.charAt(random.nextInt(ALPHA_NUM_CHARS.length())));
+            sb.append(ALPHA_NUM_CHARS.charAt(random.nextInt(ALPHA_NUM_CHARS.length())));
         }
-        return buffer.toString();
+        return sb.toString();
     }
 
     private TestHelpers() {
