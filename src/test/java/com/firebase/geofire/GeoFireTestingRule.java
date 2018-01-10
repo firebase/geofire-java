f96c10fd6b3192f7159598fca880334a9a518224
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-10 14:32:02 +0100

    Refactor RealDataTest into a GeoFireTestingRule.

--- src/test/java/com/firebase/geofire/GeoFireTestingRule.java
+++ src/test/java/com/firebase/geofire/GeoFireTestingRule.java
@@ -3,11 +3,16 @@ package com.firebase.geofire;
 import com.firebase.geofire.util.SimpleFuture;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseCredential;
 import com.google.firebase.auth.FirebaseCredentials;
 import com.google.firebase.database.*;
+import java.io.FileNotFoundException;
 import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runner.Description;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -16,19 +21,25 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class RealDataTest {
-
+public final class GeoFireTestingRule extends TestWatcher {
     private static final String DATABASE_URL = "https://geofiretest-8d811.firebaseio.com/";
     private static final String SERVICE_ACCOUNT_CREDENTIALS = "service-account.json";
 
-    DatabaseReference databaseReference;
+    private DatabaseReference databaseReference;
 
-    @Before
-    public void setup() throws IOException {
+    @Override public void starting(Description description) {
         if (FirebaseApp.getApps().isEmpty()) {
+            final FirebaseCredential credentials;
+
+            try {
+                credentials = FirebaseCredentials.fromCertificate(new FileInputStream(SERVICE_ACCOUNT_CREDENTIALS));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
             FirebaseOptions firebaseOptions = new FirebaseOptions.Builder()
                     .setDatabaseUrl(DATABASE_URL)
-                    .setCredential(FirebaseCredentials.fromCertificate(new FileInputStream(SERVICE_ACCOUNT_CREDENTIALS)))
+                    .setCredential(credentials)
                     .build();
             FirebaseApp.initializeApp(firebaseOptions);
             FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG);
@@ -37,18 +48,18 @@ public class RealDataTest {
     }
 
     public GeoFire newTestGeoFire() {
-        return new GeoFire(this.databaseReference.child(TestHelpers.randomAlphaNumericString(16)));
+        return new GeoFire(databaseReference.child(TestHelpers.randomAlphaNumericString(16)));
     }
 
-    protected void setLoc(GeoFire geoFire, String key, double latitude, double longitude) {
+    public void setLoc(GeoFire geoFire, String key, double latitude, double longitude) {
         setLoc(geoFire, key, latitude, longitude, false);
     }
 
-    protected void removeLoc(GeoFire geoFire, String key) {
+    public void removeLoc(GeoFire geoFire, String key) {
         removeLoc(geoFire, key, false);
     }
 
-    protected void setValueAndWait(DatabaseReference databaseReference, Object value) {
+    public void setValueAndWait(DatabaseReference databaseReference, Object value) {
         final SimpleFuture<DatabaseError> futureError = new SimpleFuture<DatabaseError>();
         databaseReference.setValue(value, new DatabaseReference.CompletionListener() {
             @Override
@@ -65,7 +76,7 @@ public class RealDataTest {
         }
     }
 
-    protected void setLoc(GeoFire geoFire, String key, double latitude, double longitude, boolean wait) {
+    public void setLoc(GeoFire geoFire, String key, double latitude, double longitude, boolean wait) {
         final SimpleFuture<DatabaseError> futureError = new SimpleFuture<DatabaseError>();
         geoFire.setLocation(key, new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
             @Override
@@ -84,7 +95,7 @@ public class RealDataTest {
         }
     }
 
-    protected void removeLoc(GeoFire geoFire, String key, boolean wait) {
+    public void removeLoc(GeoFire geoFire, String key, boolean wait) {
         final SimpleFuture<DatabaseError> futureError = new SimpleFuture<DatabaseError>();
         geoFire.removeLocation(key, new GeoFire.CompletionListener() {
             @Override
@@ -103,7 +114,7 @@ public class RealDataTest {
         }
     }
 
-    protected void waitForGeoFireReady(GeoFire geoFire) throws InterruptedException {
+    public void waitForGeoFireReady(GeoFire geoFire) throws InterruptedException {
         final Semaphore semaphore = new Semaphore(0);
         geoFire.getDatabaseReference().addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -119,8 +130,8 @@ public class RealDataTest {
         Assert.assertTrue("Timeout occured!", semaphore.tryAcquire(TestHelpers.TIMEOUT_SECONDS, TimeUnit.SECONDS));
     }
 
-    @After
-    public void teardown() {
+    @Override
+    public void finished(Description description) {
         this.databaseReference.setValue(null);
         this.databaseReference = null;
     }
